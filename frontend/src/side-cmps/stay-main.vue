9678a387af7c16ed1c560515c482fab6ffa19e68
baruch yakubov <baruchyaku@gmail.com>
2023-01-18 21:14:15 +0200

    added details responsiove

--- frontend/src/side-cmps/stay-main.vue
+++ frontend/src/side-cmps/stay-main.vue
@@ -1,6 +1,6 @@
 <template>
+      <images-carousel :stay="stay" :images="stay.imgUrls" :loop="false" class="details-carousel"></images-carousel>
   <div v-if="stay">
-    <images-carousel :stay="stay" :images="stay.imgUrls" :loop="false"></images-carousel>
     <h2 class="stay-details-name">{{ stay.name }}</h2>
     <div class="headerFlex flex-box">
       <div class="stay-details-basicInfo">
